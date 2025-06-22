826207796d382498bf669a959ecda913120df716
Khemmie-Ray <atokemmy@gmail.com>
2025-06-22 19:16:32 +0100

    FE integration

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import AllRoutes from "./config/AllRoutes";
+import './connection/index'
+
+function App() {
+  return <AllRoutes />;
+}
+
+export default App;
---
+++ src/connection/index.js
@@ -0,0 +1,28 @@
+import { createAppKit } from "@reown/appkit/react";
+import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+import { baseSepolia } from "@reown/appkit/networks";
+import { curtis } from "@reown/appkit/networks";
+
+// 1. Get projectId
+const projectId = import.meta.env.VITE_PROJECTID;
+
+const networks = [curtis];
+
+// 3. Create a metadata object - optional
+const metadata = {
+  name: "My Website",
+  description: "My Website description",
+  url: "http://localhost:5173", // origin must match your domain & subdomain
+  icons: ["./mark.svg"],
+};
+
+// 4. Create a AppKit instance
+createAppKit({
+  adapters: [new EthersAdapter()],
+  networks,
+  metadata,
+  projectId,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  }
+});
